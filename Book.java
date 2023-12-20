790895dc37baef1bdeb9dbe1cb6a0fdfa95ea98e
java95 <sangeetak.xworkz@gmail.com>
2023-12-21 00:41:26 +0530

    constructor1

--- Book.java
+++ Book.java
@@ -33,7 +33,22 @@ public class Book{
 		
 		
 		Book book1=new Book("coco", 250,"harry",500,2015, "West land" , "disney");
+				Book book2=new Book("deniss", 280,"harry",500,2015, "West land" , "disney");
+		Book book3=new Book("memory", 250,"richi",500,2015, "West land" , "disney");
+		Book book4=new Book("hustle", 250,"joe",500,2015, "West land" , "disney");
+		Book book5=new Book("time to kill", 250,"franc",500,2015, "West land" , "disney");
+		Book book6=new Book("happy place", 250,"michel",500,2015, "West land" , "disney");
+		Book book7=new Book("mindfullness", 250,"jonh",500,2015, "West land" , "disney");
+
 	book1.printBookDetails();
+		book2.printBookDetails();
+	book3.printBookDetails();
+	book4.printBookDetails();
+	book5.printBookDetails();
+	book6.printBookDetails();
+	book7.printBookDetails();
+
+	
 		
 	}
 	   
