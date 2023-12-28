fe50073ac1db8d97e2aa0b12dd73836288a3b95c
java95 <sangeetak.xworkz@gmail.com>
2023-12-28 08:59:41 +0530

    static

---
+++ Mobile.java
@@ -0,0 +1,78 @@
+public class Employee{
+	int empId;
+	int empSalary;
+	string empName;
+	String Designation;
+	static String ceo;
+	static String companyName;
+
+}
+public class MyCollege{
+	static String collegeName;
+	static String address;
+	static String studentDressCode;
+    int noOfStudents;
+	int noofClassrooms;
+	String subjects;
+	int idnum;
+}
+public class Mobile{
+	String brand;
+	int price;
+	String network;
+	String memory;
+	static String name; // smartphone,phone
+}
+public class Student{
+	int rollNo;
+	String name;
+	int subject;
+	static string college;
+	
+}
+public class Country{
+	static int countrycounter;
+	static String language;
+	String name;
+	int noOfStates;
+	int population;
+	int litracyRate;
+}
+public class Department{
+		static int noOfemployees;
+		String name;
+		int idNum;
+		
+}
+public class Cat{
+	static catCount;
+	 int age;
+	 String age;
+	 String breed;
+	
+}
+public class MyCollege{
+	static String collegeName;
+	static String address;
+	static String studentDressCode;
+    int noOfStudents;
+	int noofClassrooms;
+	String subjects;
+	int idnum;
+}
+{
+	public class Pen{
+		static String brand;
+		String color;
+		String type;
+		int size;
+		int price;
+}
+	public class BookTickets{
+	static int totalTickets;
+	int noOfTicket;
+	static totalOccupied;
+	int viewers;
+)
+
+	
